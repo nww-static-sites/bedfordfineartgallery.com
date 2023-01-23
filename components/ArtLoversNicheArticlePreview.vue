8eadfe577838ca7c6c71e369aaab09ea2df06ee8
Shawn Schmalz <shawns@nittanyweb.com>
2023-01-23 15:09:12 +0000

    art lovers niche tablet fix

--- components/ArtLoversNicheArticlePreview.vue
+++ components/ArtLoversNicheArticlePreview.vue
@@ -7,7 +7,8 @@
                     src="/images/Top-Banner.jpg"
                     width="397"
                     height="126"
-                    :img-attrs="{alt: 'Art Lovers Niche', style: 'width: 100%; height:auto; border:1px solid #222222;margin-bottom: 1.5em;'}"
+                    alt="Art Lovers Niche"
+                    style="width: 100%; height: auto; border: 1px solid #222222"
             /></nuxt-link>
         </div>
         <div class="highlights_prev">
@@ -33,7 +34,7 @@ export default {
         artLoversNicheArticle: {
             type: Object,
             required: true,
-        }
+        },
     },
     computed: {
         date() {
