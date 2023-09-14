62cce75c90234c2f8d8fa4e94bbce70e6b78ca7a
Adam Hopkins <violapiratejunky@gmail.com>
2023-09-14 11:15:26 -0400

    formatting

--- pages/mailing_list_thanks.vue
+++ pages/mailing_list_thanks.vue
@@ -4,14 +4,9 @@
             <section class="wrapper clearfix">
                 <div class="flex">
                     <div>
-                        <nuxt-picture provider="cloudinary"
-                            :img-attrs="{class:'art_detail_img'}"
-                            src="gallery_thanks.jpg"
-                            width="660"
-                            height="792"
-                            alt="The Lookout
-        "
-                        />
+                        <nuxt-picture provider="cloudinary" :img-attrs="{ class: 'art_detail_img' }" src="gallery_thanks.jpg"
+                            width="660" height="792" alt="The Lookout
+        " />
                     </div>
                     <div class="mobile_1">
                         <div class="artwork_header">
@@ -22,21 +17,16 @@
                             share our passion for fine art.
                         </p>
                         <p style="text-align: left; padding-top: 20px">
-                            <span style="display: block; padding-bottom: 10px; font-weight: bold"
-                                >Here's what a customer said:</span
-                            >"Thank you for including me on your mailing list. Every time I see Bedford Fine Art Gallery
+                            <span style="display: block; padding-bottom: 10px; font-weight: bold">Here's what a customer
+                                said:</span>"Thank you for including me on your mailing list. Every time I see Bedford Fine
+                            Art Gallery
                             in my mail I know I am in for a treat. "
                         </p>
                         <span style="display: block; font-weight: bold; padding-top: 10px"> Beth L. </span>
                     </div>
                     <div>
-                       <nuxt-picture provider="cloudinary"
-                            :img-attrs="{class:'art_detail_img'}"
-                            src="Jerry-and-Joan-web.jpg"
-                            width="1200"
-                            height="1652"
-                            alt=""
-                        />
+                        <nuxt-picture provider="cloudinary" :img-attrs="{ class: 'art_detail_img' }"
+                            src="Jerry-and-Joan-web.jpg" width="1200" height="1652" alt="" />
                     </div>
                 </div>
             </section>
@@ -50,21 +40,25 @@
     justify-content: space-between;
     flex-wrap: wrap;
 }
-.flex > div {
+
+.flex>div {
     width: 32%;
 }
+
 .flex img {
     width: 100%;
     height: auto;
 }
 
 @media screen and (max-width: 860px) {
-    .flex > div {
+    .flex>div {
         width: 100%;
     }
+
     .mobile_1 {
         order: -1;
     }
+
     .flex img {
         max-width: 350px;
         margin: 15px auto;
