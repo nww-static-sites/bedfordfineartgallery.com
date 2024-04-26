5f9c37637ba85c88ce460b8b53f75b7606b05eda
Shawn Schmalz <shawns@nittanyweb.com>
2024-04-26 12:44:45 -0400

    april 26 homepage demo

---
+++ components/GallerySlidingImages.vue
@@ -0,0 +1,136 @@
+<template>
+    <div>
+        <VueSlickCarousel v-bind="settings">
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_1.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_2.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_3.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_4.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_5.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_6.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_7.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_8.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_9.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_10.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_11.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_12.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_13.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_14.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_15.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_16.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_17.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_18.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_19.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_20.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_21.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_22.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_23.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_24.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_25.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_26.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_27.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_28.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_29.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_30.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_31.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_32.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_33.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_34.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_35.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_36.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_37.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_38.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_39.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_40.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_41.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_42.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_43.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_44.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_45.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_46.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_47.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_48.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_49.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_50.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_51.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_52.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_53.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_54.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_55.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_56.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_57.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_58.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_59.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_60.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_61.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_62.jpg" /></div>
+            <div><nuxt-picture provider="cloudinary" src="/gallery-sliding-images/gallery_slider_63.jpg" /></div>
+
+                  
+                 
+
+
+
+
+
+
+
+
+
+
+
+        </VueSlickCarousel>
+    </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+    name: 'MyComponent',
+    components: { VueSlickCarousel },
+    data() {
+        return {
+            settings: {
+                arrows: false,
+                dots: false,
+                infinite: true,
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                autoplay: true,
+                speed: 9000,
+                autoplaySpeed: 2000,
+                cssEase: 'linear',
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true,
+                        },
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            initialSlide: 2,
+                        },
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        },
+                    },
+                ],
+            },
+        }
+    },
+}
+</script>
