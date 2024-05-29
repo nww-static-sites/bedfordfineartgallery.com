0f4a90d3d8559164d35f73312db3e99b398e2e0d
Shawn Schmalz <shawns@nittanyweb.com>
2024-05-29 07:47:39 -0400

    new customer image

--- components/CustomerSlidingImages.vue
+++ components/CustomerSlidingImages.vue
@@ -86,6 +86,7 @@
                   
                   <div><nuxt-picture provider="cloudinary" src="/customer-images/82.jpg" /></div>
                   <div><nuxt-picture provider="cloudinary" src="/customer-images/83.jpg" /></div>
+                  <div><nuxt-picture provider="cloudinary" src="/customer-images/84.jpg" /></div>
                   
                  
 
